fc76d6ddaf0c18abb94243f1449d10ea3d6f2dfa
xqz-u <marco.gallo0530@gmail.com>
2026-07-09 11:14:07 +0200

    chore: formatting.

--- lua/plugins/coding/lsp-mason.lua
+++ lua/plugins/coding/lsp-mason.lua
@@ -79,7 +79,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
       client.server_capabilities.renameProvider = false -- ty owns rename
     end
     if client and client:supports_method('textDocument/documentHighlight', event.buf) then
-      local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+      local highlight_augroup =
+        vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
         buffer = event.buf,
         group = highlight_augroup,
@@ -103,7 +104,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
     --
     -- This may be unwanted, since they displace some of your code
     if client and client:supports_method('textDocument/inlayHint', event.buf) then
-      map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }) end, '[T]oggle Inlay [H]ints')
+      map(
+        '<leader>th',
+        function()
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+        end,
+        '[T]oggle Inlay [H]ints'
+      )
     end
   end,
 })
@@ -142,7 +149,12 @@ local servers = {
       client.server_capabilities.documentFormattingProvider = false -- Disable formatting (formatting is done by stylua)
       if client.workspace_folders then
         local path = client.workspace_folders[1].name
-        if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+        if
+          path ~= vim.fn.stdpath 'config'
+          and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+        then
+          return
+        end
       end
       client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
         runtime = {
