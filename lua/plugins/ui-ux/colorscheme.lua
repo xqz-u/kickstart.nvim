2a22c3c8526dfe6766aab454bf5c43787fec394e
xqz-u <marco.gallo0530@gmail.com>
2026-07-09 11:13:46 +0200

    chore: better default colorscheme.

--- lua/plugins/ui-ux/colorscheme.lua
+++ lua/plugins/ui-ux/colorscheme.lua
@@ -18,4 +18,4 @@ require('tokyonight').setup {
 -- Load the colorscheme here.
 -- Like many other themes, this one has different styles, and you could load
 -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-vim.cmd.colorscheme 'tokyonight-night'
+vim.cmd.colorscheme 'tokyonight-moon'
